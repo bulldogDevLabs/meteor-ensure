43145d38a86391d7e5f410b9ae1dd11e415a14a1
elosoman <bdeptola@gmail.com>
2015-04-15 00:33:57 -0400

    Create package.js

---
+++ package.js
@@ -0,0 +1,26 @@
+
+
+Package.describe({
+
+  name: 'elosoman:meteor-ensure',
+  summary: "Mongo index management",
+  version: "0.0.1",
+  git: "https://github.com/elosoman/meteor-ensure.git"
+
+});
+
+Package.onUse(function(api) {
+
+  api.versionsFrom('1.0.3.2');
+
+  api.use([
+    'matb33:collection-hooks@0.7.11',
+    'dburles:mongo-collection-instances@0.3.3',
+    'underscore'
+  ]);
+  
+});
+
+Package.onTest(function(api) {
+	// TBD
+});
